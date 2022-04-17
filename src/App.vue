26135ff70664f7692cf0413f6b5200debb32703e
Evan <evanz1215@gmail.com>
2022-04-18 00:36:57 +0800

    tailwindv3 install

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="text-indigo-600 text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
